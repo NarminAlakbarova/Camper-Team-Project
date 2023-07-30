e55a4ec8103fa5b0e163549af6d9164b6578ab0e
SevilHs <7l2zfk3@code.edu.az>
2023-07-31 00:32:59 +0400

    feat: header is almost done

--- client/src/layout/header/index.jsx
+++ client/src/layout/header/index.jsx
@@ -1,18 +1,59 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/header/logo.png";
+import "./index.scss";
+import { Dropdown, Space } from "antd";
+import DropdownPages from "../../components/DropdownPages";
+
+const items = [
+  {
+    label: <NavLink to={"signIn"}>Sign In</NavLink>,
+    key: "1",
+  },
+  {
+    label: <NavLink to={"signUp"}>Sign Up</NavLink>,
+    key: "2",
+  },
+];
 
 const Header = () => {
   return (
-    <div>
+    <header>
+      <div className="container">
         <nav>
-            <NavLink to={"home"}>Home</NavLink>
-            <NavLink to={"about"}>About</NavLink>
-            <NavLink to={"allTours"}>All Tours</NavLink>
-            <NavLink to={"gallery"}>Gallery</NavLink>
-            <NavLink to={"contact"}>Contact</NavLink>
+          <Link to={"/"}>
+            <img src={logo} alt="website logo" />
+          </Link>
+          <ul>
+            <li>
+              <NavLink to={"home"}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to={"about"}>About</NavLink>
+            </li>
+            <li>
+              <NavLink to={"allTours"}>All Tours</NavLink>
+            </li>
+            <li>
+              <DropdownPages />
+            </li>
+            <li>
+              <NavLink to={"contact"}>Contact</NavLink>
+            </li>
+          </ul>
+          <Dropdown
+            menu={{
+              items,
+            }}
+          >
+            <Link>
+              <Space>Join Us</Space>
+            </Link>
+          </Dropdown>
         </nav>
-    </div>
-  )
-}
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
