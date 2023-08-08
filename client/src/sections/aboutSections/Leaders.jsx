03daac20d37f195edfa9f6c82109fb0089c2be8b
NarminAlakbarova <735e5lb@code.edu.az>
2023-08-08 20:41:52 +0400

    style: UI changed

--- client/src/sections/aboutSections/Leaders.jsx
+++ client/src/sections/aboutSections/Leaders.jsx
@@ -3,11 +3,23 @@ import leader1 from "../../assets/img/about/thumbnail-1.jpg";
 import leader2 from "../../assets/img/about/thumbnail-2.jpg";
 import leader3 from "../../assets/img/about/thumbnail-3.jpg";
 import leader4 from "../../assets/img/about/thumbnail-4.jpg";
+import { BiLogoFacebook } from "react-icons/bi";
+import { FaLinkedinIn } from "react-icons/fa";
+import { BiLogoPinterestAlt } from "react-icons/bi";
+import { AiOutlineTwitter } from "react-icons/ai";
 
 const LeaderCards = ({ name, status, img }) => (
-  <div className="col-xl-3 col-md-4 my-3">
+  <div className="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-xs-6  my-3">
     <div className="card">
       <div className="img">
+        <div className="overlay">
+          <div className="icons">
+            <BiLogoFacebook />
+            <FaLinkedinIn />
+            <BiLogoPinterestAlt />
+            <AiOutlineTwitter />
+          </div>
+        </div>
         <img src={img} alt="" />
       </div>
       <div className="content">
@@ -23,7 +35,7 @@ const Leaders = () => {
     <div id="leaders">
       <div className="container">
         <div className="leaders">
-          <p className="heading">Meet Our Leaders</p>
+          <p className="title">Meet Our Leaders</p>
           <div className="row">
             <LeaderCards
               img={leader4}
