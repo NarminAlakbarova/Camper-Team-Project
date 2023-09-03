45a02546aec0eaa62f05fa370ebf4cd812bc5788
SevilHs <7l2zfk3@code.edu.az>
2023-09-04 02:18:20 +0400

    feat: admin news page added

--- client/src/admin/root/index.jsx
+++ client/src/admin/root/index.jsx
@@ -2,21 +2,22 @@ import React, { useState } from "react";
 import Aside from "../layouts/aside";
 import { Outlet } from "react-router-dom";
 import SignIn from "../pages/signIn";
+import { Provider } from "react-redux";
+import { store } from "../../redux/store";
 
 const AdminRoot = () => {
   const [checkAdmin, setCheckAdmin] = useState(true);
   return (
-    <>
+    <Provider store={store}>
       {!checkAdmin ? (
         <SignIn setCheckAdmin={setCheckAdmin} checkAdmin={checkAdmin}/>
       ) : (
         <>
-        
           <Aside />
           <Outlet />
         </>
       )}
-    </>
+    </Provider>
   );
 };
 
