b48e1a2b862e094798f6784c3ce991eeba48970d
SevilHs <7l2zfk3@code.edu.az>
2023-09-07 01:14:27 +0400

    users data added & sign up is done

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import newsDataReducer from "./newsDataSlice";
 import feedBackDataReducer from "./feedBackDataSlice";
 import toursDataReducer from "./toursDataSlice";
+import usersDataReducer from "./usersDataSlice";
 
 export const store = configureStore({
   reducer: {
     newsData: newsDataReducer,
     feedBackData: feedBackDataReducer,
     toursData: toursDataReducer,
+    usersData: usersDataReducer
   },
 });
---
+++ client/src/redux/usersDataSlice.js
@@ -0,0 +1,37 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8080";
+const initialState = {
+  data: [],
+  loading: false,
+  error: "",
+};
+
+export const getUsersData = createAsyncThunk("getUsersData", async () => {
+  const res = await axios(`${BASE_URL}/users`);
+  return res.data;
+});
+
+export const addUser = createAsyncThunk("addUser", async (userInfo) => {
+  await axios.post(`${BASE_URL}/users`,userInfo);
+});
+
+export const usersDataSlice = createSlice({
+  name: "usersData",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getUsersData.pending, (state) => {
+      (state.loading = true), (state.error = "");
+    }),
+    builder.addCase(getUsersData.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+    });
+    builder.addCase(getUsersData.rejected, (state) => {
+      state.error = "Data not Found";
+    });
+  },
+});
+
+export default usersDataSlice.reducer;
