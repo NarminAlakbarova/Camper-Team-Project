376eba94c8e43641903b8cfc46ec31e6ffe1c669
NarminAlakbarova <735e5lb@code.edu.az>
2023-09-18 00:48:43 +0400

    added context for users

--- client/src/validation/SignInValidation.jsx
+++ client/src/validation/SignInValidation.jsx
@@ -3,7 +3,6 @@ import * as Yup from "yup";
 export const SignInValidation = Yup.object().shape({
   userName: Yup.string()
     .min(2, "User Name must be 2 or more characters!")
-    .max(10, "User Name must be 10 or less!")
     .required("User Name must be required!"),
   password: Yup.string()
     .min(6, "Password must be 6 or more characters!")
